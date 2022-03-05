1df5d08be4b316a5aa5c956e950d416329f5eb3d
Parag-git10 <87827519+Parag-git10@users.noreply.github.com>
2022-03-05 20:59:08 +0530

    Push command line argument

--- TwoArgs.java
+++ TwoArgs.java
@@ -1,7 +1,7 @@
 /*Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 
- *
- *Consider,
- *If the two command line arguments are Ramesh and Rajesh then the output generated should be Ramesh and Rajesh.
+ 
+ Consider,
+ If the two command line arguments are Ramesh and Rajesh then the output generated should be Ramesh and Rajesh.
  */
 
 import java.util.*;
